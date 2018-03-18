e47279067b462ab0d8a5bb1a6bcfc1b407bac4fb
Sumanathilaka <37451505+Sumanathilaka@users.noreply.github.com>
2018-03-18 15:39:20 +0530

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index.php</title>
+</head>
+<body>
+<center>
+	<h1>Welcome to NITC PHD student project management System</h1>
+<h2>SWE Students project</h2>
+</center>
+</body>
+</html>
